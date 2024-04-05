dd2958f36a415f0f8adf5a28d3de95484e57d373
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2024-04-04 21:06:08 -0600

    Update project1-libc-redo.c
    This doesn't take in anything from the keyboard and at every ecall it just prints an unrecognized symbol.

--- project1-libc-redo.c
+++ project1-libc-redo.c
@@ -8,21 +8,21 @@
 .text
 
 main:
-    # call puts(*str)
+    # Print the prompt message
     la a0, prompt         # Put string pointer (prompt) into a0
-    jal ra, puts           # Call puts
+    jal ra, puts          # Call puts
 
-    # call gets(*buf)
+    # Get input from the user
     la a0, buf            # Put address pointer (buf) into a0
-    jal ra, gets           # Call gets
+    jal ra, gets          # Call gets and read user input
 
-    # call puts(*buf)
+    # Print the input message
     la a0, buf            # Put string pointer (buf) into a0
-    jal ra, puts           # Call puts
+    jal ra, puts          # Call puts to print the input message
 
-    # exit program
-    li a0, 0               # Put 0 into a0 (return 0)
-    li a7, __NR_EXIT       # Put NR_EXIT code into a7
+    # Exit the program
+    li a0, 0              # Put 0 into a0 (return 0)
+    li a7, __NR_EXIT      # Put NR_EXIT code into a7
     ecall
 
 halt:
@@ -30,98 +30,48 @@ halt:
     j halt                 # stop execution
 
 getchar:
-    # prolog
-    addi sp, sp, -4        # make room for 1 item on the stack
-
-    # body
     li a0, STDIN           # Put STDIN code into a0
-    mv a1, sp              # Put sp into a1 (a place to store the read-in char)
     li a2, 1               # Put value 1 into a2 (read in one byte)
     li a7, __NR_READ       # Put NR_READ code into a7
     ecall
-
-    # epilog
-    lw a0, 0(sp)           # load *sp into a0 (return ascii value)
-    addi sp, sp, 4         # restore sp
     ret
 
 putchar:
-    # prolog
-    addi sp, sp, -4        # make room for 1 item on the stack
-    sw a0, 0(sp)           # store a0 into stack (save input char into memory)
-
-    # body
     li a0, STDOUT          # Put STDOUT code into a0
-    mv a1, sp              # Put sp into a1 (pointer to input char)
     li a2, 1               # Put value 1 into a2 (write 1 byte)
     li a7, __NR_WRITE      # Put NR_WRITE code into a7
     ecall
-
-    # epilog
-    lw a0, 0(sp)           # load (byte value) of *sp back into a0
-    addi sp, sp, 4         # restore sp
     ret
 
 gets:
-    # prolog
-    addi sp, sp, -12       # make room for 3 items on the stack
-    sw ra, 0(sp)           # save ra
-    sw a0, 4(sp)           # save a0
-    sw s0, 8(sp)           # save s0
-
-    # body
-    mv s0, a0              # move a0 into s0 (use s0 to increment the addr pointer)
+    mv t0, a0              # Move a0 into t0 (use t0 to increment the address pointer)
+    li t1, 100             # Maximum buffer size
 gets_loop:
     jal ra, getchar        # Call getchar
-    mv t0, a0              # Store the read-in character in t0
-    blt t0, zero, gets_error # If read-in char is negative, goto gets_error
-    sb t0, 0(s0)           # Store the read-in character at the address pointer
-    addi s0, s0, 1         # Increment s0 by 1
-    li t1, 10              # Load the ASCII value of newline into a temporary register (t1)
-    beq t0, t1, gets_exit # Branch to gets_exit if read-in char is newline
-    j gets_loop            # Continue loop if not newline
-
-gets_error:
-    li a0, -1              # Put -1 into a0 (return -1)
-    j epilog_gets
+    sb a0, 0(t0)           # Store the read-in character at the address pointer
+    addi t0, t0, 1         # Increment t0 by 1
+    addi t1, t1, -1        # Decrement buffer size counter
+    beqz a0, gets_exit     # If read-in char is newline, exit loop
+    j gets_loop            # Continue loop
 
 gets_exit:
-    sb zero, 0(s0)         # End the input string with a terminating 0 byte
-    j epilog_gets
-
-epilog_gets:
-    lw ra, 0(sp)           # restore ra
-    lw a0, 4(sp)           # restore a0
-    lw s0, 8(sp)           # restore s0
-    addi sp, sp, 12        # restore stack pointer
+    sb zero, 0(t0)         # End the input string with a terminating 0 byte
+    mv a0, t0              # Put the address of the null-terminated string into a0
     ret
 
 puts:
-    # prolog
-    addi sp, sp, -4        # make room for 1 item on the stack
-    sw ra, 0(sp)           # save ra
-
-    # body
-    mv s0, a0              # move a0 into s0 (use s0 to increment the addr pointer)
+    mv t0, a0              # Move a0 into t0 (use t0 to increment the address pointer)
 puts_loop:
-    lb t0, 0(s0)           # Put the ascii value that s0 is currently pointing to into t0
-    beqz t0, puts_exit     # if ascii value is zero (null char), goto to puts_exit
+    lb a0, 0(t0)           # Load the byte that t0 is currently pointing to into a0
+    beqz a0, puts_exit     # If a0 is zero (null character), exit loop
     jal ra, putchar        # Call putchar
-    addi s0, s0, 1         # Increment string pointer s0
-    bgez t0, puts_loop     # If t0 >= 0, goto puts_loop
-
-puts_error:
-    li a0, -1              # Put -1 into a0 (return -1)
-    j epilog_puts
+    addi t0, t0, 1         # Increment string pointer t0
+    j puts_loop            # Continue loop
 
 puts_exit:
-    li a0, newline         # Put newline char into a0 (value 10)
-    jal ra, putchar        # Call putchar
+    li a0, 10              # Put newline char into a0 (value 10)
+    jal ra, putchar        # Call putchar to print newline
     li a0, 0               # Put 0 into a0 (return 0)
-
-epilog_puts:
-    lw ra, 0(sp)           # restore ra
-    addi sp, sp, 4         # restore stack pointer
     ret
 
 .data
