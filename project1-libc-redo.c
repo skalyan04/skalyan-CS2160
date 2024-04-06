948b7306989c8141efc57a1f60af5fe227e3b742
skalyan04 <123116625+skalyan04@users.noreply.github.com>
2024-04-05 19:34:42 -0600

    Update project1-libc-redo.c
    IT WORKS!!!! IT WORKS WEIRDLY BUT IT WORKSS!!!

--- project1-libc-redo.c
+++ project1-libc-redo.c
@@ -1,4 +1,4 @@
-.globl main 
+.globl main
 .equ STDOUT, 1
 .equ STDIN, 0
 .equ __NR_READ, 63
@@ -6,40 +6,21 @@
 .equ __NR_EXIT, 93
 
 .text
+
 main:
-	# main() prolog
-	addi sp, sp, -24
-	sw ra, 20(sp)
-
-	# main() body
-	la a0, prompt
-	call puts
-
-	mv a0, sp
-	call gets
-
-	mv a0, sp
-	call puts
-
-	# main() epilog
-	lw ra, 20(sp)
-	addi sp, sp, 24
-	ret
-
-.space 12288
-
-sekret_fn:
-	addi sp, sp, -4
-	sw ra, 0(sp)
-	la a0, sekret_data
-	call puts
-	lw ra, 0(sp)
-	addi sp, sp, 4
-	ret
-
-##############################################################
-# Add your implementation of puts() and gets() below here
-##############################################################
+    # Call puts to display the prompt message
+    la a0, prompt         # Load string pointer (prompt) into a0
+    jal ra, puts          # Call puts
+    # Call gets to read input from the user
+    la a0, buf            # Load address pointer (buf) into a0
+    jal ra, gets          # Call gets
+    # Call puts to display the input message
+    la a0, buf            # Load string pointer (buf) into a0
+    jal ra, puts          # Call puts
+    # Exit program
+    li a0, 0              # Put 0 into a0 (return 0)
+    li a7, __NR_EXIT      # Put NR_EXIT code into a7
+    ecall
 
 getchar:
     addi sp, sp, -4         # Make room for 1 item on the stack
@@ -87,7 +68,6 @@ epilog_gets:
     addi sp, sp, 12         # Restore stack pointer
     ret
 
-
 putchar:
     # Prolog
     addi sp, sp, -4         # Make room for 1 item on the stack
@@ -133,13 +113,8 @@ puts_exit:
     addi sp, sp, 12         # Restore stack pointer
     ret
 
-##############################################################
-
 .data
 prompt:   .ascii  "Enter a message: "
 prompt_end:
 newline: .byte 10
-
-.word 0
-sekret_data:
-.word 0x73564753, 0x67384762, 0x79393256, 0x3D514762, 0x0000000A
+buf: .space 10
